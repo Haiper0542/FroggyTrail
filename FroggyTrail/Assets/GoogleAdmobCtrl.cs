ff1323a744e0304b0b3f30dc515ac670eb381437
= <=>
2019-04-13 12:21:51 +0900

    FroggyTrail

---
+++ FroggyTrail/Assets/GoogleAdmobCtrl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleAdmobCtrl : MonoBehaviour {
+
+    private BannerView bannerView;
+
+    void Awake()
+    {
+        string appId = "ca-app-pub-5023911560716306~3018371852";
+
+        MobileAds.Initialize(appId);
+    }
+
+    public void CheckNoAds()
+    {
+        RequestBanner();
+        ShowBannerAd();
+    }
+
+    public void ShowBannerAd()
+    {
+        bannerView.Show();
+    }
+
+    public void HideBannerAd()
+    {
+        bannerView.Hide();
+    }
+
+    private void RequestBanner()
+    {
+        string adUnitId = "ca-app-pub-5023911560716306/2252085091";
+
+        bannerView = new BannerView(adUnitId, AdSize.SmartBanner, AdPosition.Bottom);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        bannerView.LoadAd(request);
+    }
+}
